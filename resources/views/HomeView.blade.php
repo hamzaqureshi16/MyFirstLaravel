ba992619944c0217512438418435b33f88beb2a0
Hamza Qureshi <m.hamzaqureshi15@gmail.com>
2023-02-23 11:36:17 +0500

    initial commit

---
+++ resources/views/HomeView.blade.php
@@ -0,0 +1,13 @@
+<!-- resources/views/home.blade.php -->
+
+@extends('Navbar');
+
+@section('content')
+  <div class="center d-inline">
+    <h1 class="text-light p-2 rounded bg-dark ">This is my First View</h1>
+  </div>
+@endsection
+
+
+
+    
---
+++ routes/web.php
@@ -0,0 +1,33 @@
+<?php
+
+use App\Http\Controllers\Controller;
+use App\Http\Controllers\PostController;
+use Barryvdh\Debugbar\Facades\Debugbar;
+use Barryvdh\Debugbar\Twig\Extension\Debug;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/',[PostController::class,'index']);
+Route::get('/about',[PostController::class,'about']);
+
+// Route::get('/', function () {
+//     return [PostController::class,'index'];
+//     return view('HomeView');
+// });
+
+// Route::get('/about', function () {
+//     return view('welcome');
+// });
+
+// //Route::get('/',[PostController::class,'index']);
+// Route::resource('user', PostController::class);
